acc1a999eba2b5596cd31d97996d43dca5ed04ff
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-05-05 17:49:59 +0100

    LoginForm

---
+++ formulario/formulario/loginform.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="loginform.aspx.cs" Inherits="formulario.loginform" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Formulário de login</title>    
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Nome:
+            <asp:TextBox ID="tb_nome" runat="server" Width="571px"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator_nome" runat="server" ControlToValidate="tb_nome" ErrorMessage="Nome obrigatório" Font-Bold="True" ForeColor="Red">*</asp:RequiredFieldValidator>
+            <br />
+            <br />
+            Morada: <asp:TextBox ID="tb_morada" runat="server" TextMode="MultiLine" Width="548px"></asp:TextBox>
+            <br />
+            <br />
+            Email:
+            <asp:TextBox ID="tb_email" runat="server" TextMode="Email" Width="556px"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator_email" runat="server" ControlToValidate="tb_email" ErrorMessage="Email obrigatório" Font-Bold="True" ForeColor="Red">*</asp:RequiredFieldValidator>
+            <asp:RegularExpressionValidator ID="RegularExpressionValidator_email" runat="server" ControlToValidate="tb_email" ErrorMessage="Email inválido" Font-Bold="True" ForeColor="#00CC00" ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*">*</asp:RegularExpressionValidator>
+            <br />
+            <br />
+            Código Postal:
+            <asp:TextBox ID="tb_codpostal" runat="server" Width="321px"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator_codpostal" runat="server" ControlToValidate="tb_codpostal" ErrorMessage="Código postal obrigatório" Font-Bold="True" ForeColor="Red">*</asp:RequiredFieldValidator>
+            <asp:RegularExpressionValidator ID="RegularExpressionValidator_codpostal" runat="server" ControlToValidate="tb_codpostal" ErrorMessage="Código postal inválido" Font-Bold="True" ForeColor="#00CC00" ValidationExpression="^\d{4}(-\d{3})?$">*</asp:RegularExpressionValidator>
+            <br />
+            <asp:ValidationSummary ID="ValidationSummary1" runat="server" DisplayMode="List" ForeColor="Red" Height="100px" Width="680px" />
+            <br />
+            <asp:Button ID="btn_enviar" runat="server" Text="Enviar" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btn_excel" runat="server" OnClick="btn_excel_Click" Text="Excel" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btn_word" runat="server" OnClick="btn_word_Click" Text="Word" />
+        &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btn_xml" runat="server" OnClick="btn_xml_Click" Text="XML" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ formulario/formulario/loginform.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Reflection;
+
+namespace formulario
+{
+    public partial class loginform : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_excel_Click(object sender, EventArgs e)
+        {
+            Response.Clear(); //limpa tudo o que está na memória
+            Response.AddHeader("Content-Disposition", "attachement; filename=ficheiro.xls");
+            Response.ContentType = "application/vnd.ms-excel"; //tipo de ficheiro em excel
+                                                                       // Obter um objeto Encoder para codificar o texto em UTF-8
+            Response.BinaryWrite(System.Text.Encoding.UTF8.GetPreamble());//get preamble vai reconhecer os caracteres
+
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter htw = new HtmlTextWriter(sw);
+
+            htw.Write("<table>" + "<tr><td><strong>Nome</strong></td><td>" + tb_nome.Text + "</td></tr>" + "<tr><td><strong>Morada</strong></td><td>" + tb_morada.Text + "</td></tr>" + "<tr><td><strong>Email</strong></td><td>" + tb_email.Text + "</td></tr>");
+
+            Response.Write(sw.ToString());
+            Response.End();
+
+        }
+
+        protected void btn_word_Click(object sender, EventArgs e)
+        {
+            Response.Clear(); //limpa tudo o que está na memória
+            Response.AddHeader("Content-Disposition", "attachement; filename=ficheiro.doc");
+            Response.ContentType = "application/vnd.ms-word"; //tipo de ficheiro em word
+                                                               // Obter um objeto Encoder para codificar o texto em UTF-8
+            Response.BinaryWrite(System.Text.Encoding.UTF8.GetPreamble());//get preamble vai reconhecer os caracteres
+
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter htw = new HtmlTextWriter(sw);
+
+            htw.Write("<table>" + "<tr><td><strong>Nome</strong></td><td>" + tb_nome.Text + "</td></tr>" + "<tr><td><strong>Morada</strong></td><td>" + tb_morada.Text + "</td></tr>" + "<tr><td><strong>Email</strong></td><td>" + tb_email.Text + "</td></tr>");
+
+            Response.Write(sw.ToString());
+            Response.End();
+
+        }
+
+        protected void btn_xml_Click(object sender, EventArgs e)
+        {
+            Response.Clear();
+            Response.AddHeader("Content-Disposition", "attachment; filename=ficheiro.xml");
+            Response.ContentType = "text/xml";
+
+            //Instancia 
+            StringWriter sw = new StringWriter();
+            //vai traduzir para o html
+            HtmlTextWriter htmlWrite = new HtmlTextWriter(sw);
+
+            htmlWrite.Write("<dados>");//nome que se dá 
+            htmlWrite.Write("<nome>" + tb_nome.Text + "</nome>");
+            htmlWrite.Write("<morada>" + tb_morada.Text + "</morada>");
+            htmlWrite.Write("<email>" + tb_email.Text + "</email>");
+            htmlWrite.Write("<codpostal>" + tb_codpostal.Text + "</codpostal>");
+            htmlWrite.Write("</dados>");
+
+            Response.Write(sw.ToString());
+            Response.End();
+
+        }
+    }
+}
